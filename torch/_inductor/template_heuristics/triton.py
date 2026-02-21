74fafdbd08fd8f8742846c32d617c72d82adbee7
Umesh Chand <umechand@amd.com>
2026-02-21 01:05:07 +0000

    Fixed a typo

--- torch/_inductor/template_heuristics/triton.py
+++ torch/_inductor/template_heuristics/triton.py
@@ -1945,7 +1945,8 @@ class MMTemplateConfigMixin(GemmMaxAutotuneTemplateConfigHeuristics):
                 dtype,
                 dtype,
                 dtype,
-                device.a_stride,
+                device,
+                a_stride,
                 b_stride,
                 c_stride,
             )
